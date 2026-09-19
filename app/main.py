45220b2c55a20d0786ccaf5b472365e6b6160096
naveen007-sekar <naveensekar0002@gmail.com>
2026-09-19 15:12:37 +0530

    updated project codes

--- app/main.py
+++ app/main.py
@@ -2,5 +2,5 @@ import logging
 from fastapi import FastAPI
 from app.api.routes import router
 logging.basicConfig(level=logging.INFO, format="%(levelname)s %(name)s: %(message)s")
-app=FastAPI(title="Social Commerce Post Recommender",version="1.0.0")
+app=FastAPI(title="Social Commerce Post Recommender",version="1.0.0",docs_url="/docs",redoc_url="/redoc")
 app.include_router(router)
